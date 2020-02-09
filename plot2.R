44daa2f072d68d969128ddf7bd5fc44c40434ccb
Robert M Lee <giradon@gmail.com>
2020-02-08 21:47:46 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,2 +1,3 @@
+#Time series plot of global minute-averaged active power (in kilowatt)
 plot(x = householdpower$Date_Time, y = householdpower$Global_active_power, type = "l", 
     xlab = "", ylab = "Global Active Power (kilowatts)")
